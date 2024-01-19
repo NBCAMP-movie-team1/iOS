555cd6c02b46dccad994fbdd6219a17e11304f32
yenny <yennydev42@gmail.com>
2024-01-19 15:44:12 +0900

    [Chore] #34 - 영화 목록 이미지 설정 변경, 제목 삭제

--- NBCAMP-movie-team1/NBCAMP-movie-team1/Presentation/View/MovieList/MovieListCollectionViewCell.swift
+++ NBCAMP-movie-team1/NBCAMP-movie-team1/Presentation/View/MovieList/MovieListCollectionViewCell.swift
@@ -13,28 +13,17 @@ class MovieListCollectionViewCell: UICollectionViewCell {
     
     private let imageView: UIImageView = {
         let imageView = UIImageView()
-        imageView.contentMode = .scaleAspectFill
-        imageView.clipsToBounds = true
+        imageView.contentMode = .scaleAspectFit
         imageView.backgroundColor = .systemGray6
         imageView.translatesAutoresizingMaskIntoConstraints = false
         
         return imageView
     }()
     
-    private let label: UILabel = {
-        let label = UILabel()
-        label.textAlignment = .left
-        label.numberOfLines = 0
-        label.font = UIFont.systemFont(ofSize: 14)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        
-        return label
-    }()
-    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        setUI() // setUI 메서드 호출
+        setUI()
     }
     
     required init?(coder: NSCoder) {
@@ -44,25 +33,18 @@ class MovieListCollectionViewCell: UICollectionViewCell {
     func configure(with movie: MovieList) {
         let link = "https://image.tmdb.org/t/p/w500\(movie.imagePath)"
         ImageLoader.loadImage(from: link, into: self.imageView)
-        label.text = movie.title
     }
 }
 
 extension MovieListCollectionViewCell {
     private func setUI() {
-        let stackView = UIStackView(arrangedSubviews: [imageView, label])
-        stackView.axis = .vertical
-        stackView.spacing = 5
-        stackView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(imageView)
 
-        contentView.addSubview(stackView) // contentView에 추가
-
-        // 스택뷰 제약 조건 설정
         NSLayoutConstraint.activate([
-            stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-            stackView.topAnchor.constraint(equalTo: contentView.topAnchor),
-            stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-            stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
         ])
     }
 }
